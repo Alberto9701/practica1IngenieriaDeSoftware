7ae6768c90a5e164699d5fa7a5bb53173e769e0d
Roberto-coder <61215751+Roberto-coder@users.noreply.github.com>
2024-09-26 11:36:16 -0600

    Inicio del proyecto
    Uso de gradle y groovy

---
+++ cerraduraWEB/src/main/groovy/ipn/mx/cerraduraweb/CerraduraWebApplication.groovy
@@ -0,0 +1,23 @@
+package ipn.mx.cerraduraweb
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@SpringBootApplication
+class CerraduraWebApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(CerraduraWebApplication, args)
+	}
+
+}
+
+@RestController
+class HelloController {
+    @GetMapping("/")
+    String hello() {
+        return "Hello, Groovy with Spring Boot!"
+    }
+}
---
+++ cerraduraWEB/src/test/groovy/ipn/mx/cerraduraweb/CerraduraWebApplicationTests.groovy
@@ -0,0 +1,13 @@
+package ipn.mx.cerraduraweb
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CerraduraWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
